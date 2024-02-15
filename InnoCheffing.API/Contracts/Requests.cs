98308fabd2516c86c9246e4f82dcb97478272d80
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-02-15 14:00:11 +0100

    Added validation attributes to request models

--- InnoCheffing.API/Contracts/Requests.cs
+++ InnoCheffing.API/Contracts/Requests.cs
@@ -1,4 +1,6 @@
-﻿namespace InnoCheffing.API.Contracts;
-public record IngredientRequest(string Name);
-public record RecipeCategoryRequest(string Name);
-public record RecipeRequest(string Name, string? Description, Guid RecipeCategoryId);
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InnoCheffing.API.Contracts;
+public record IngredientRequest([Required][MaxLength(150)] string Name);
+public record RecipeCategoryRequest([Required][MaxLength(150)] string Name);
+public record RecipeRequest([Required][MaxLength(150)] string Name, [MaxLength(500)] string? Description,[Required] Guid RecipeCategoryId);
